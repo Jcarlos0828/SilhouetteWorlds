8bb6aa3c0286248d4bce1615873db5fdb396b3b0
Jcarlos0828 <jose.carlos4013@gmail.com>
2024-06-30 18:44:09 -0600

    Configured firebase dependencies

--- firebaseApp.js
+++ firebaseApp.js
@@ -1,22 +1,26 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { initializeFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDW-wRbXlab5P1fyuX6bKM-b8ShRD3jBac",
-  authDomain: "fern-template-3e1e3.firebaseapp.com",
-  databaseURL: "https://fern-template-3e1e3-default-rtdb.firebaseio.com",
-  projectId: "fern-template-3e1e3",
-  storageBucket: "fern-template-3e1e3.appspot.com",
-  messagingSenderId: "445471113539",
-  appId: "1:445471113539:web:9dc3414964804c81c56ff6",
+  apiKey: "AIzaSyCq-tlhcInx5o9jx1vINlVbAfFKJZ9KF68",
+  authDomain: "silhouette-of-worlds.firebaseapp.com",
+  // databaseURL: "https://fern-template-3e1e3-default-rtdb.firebaseio.com",
+  projectId: "silhouette-of-worlds",
+  storageBucket: "silhouette-of-worlds.appspot.com",
+  messagingSenderId: "561002087999",
+  appId: "1:561002087999:web:b8d378701cda963ec49a8a",
+  measurementId: "G-1D7CJRS0GG",
 };
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 initializeFirestore(firebaseApp, { ignoreUndefinedProperties: true });
 
 export { firebaseApp };
